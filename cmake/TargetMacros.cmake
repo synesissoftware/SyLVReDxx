b9c5c3d14a79b3024a81712418e26760386cf345
Matt Wilson <matthew.wilson.synesis@gmail.com>
2025-05-09 10:34:48 +1000

    + added **CLASP**, **libCLImate**, and **recls** dependencies

--- cmake/TargetMacros.cmake
+++ cmake/TargetMacros.cmake
@@ -44,12 +44,15 @@ function(define_automated_test_program program_name entry_point_source_name)
 	target_link_libraries(${program_name}
 		PRIVATE
 			core
-			$<$<BOOL:b64_FOUND>:b64::core>
+			# $<$<BOOL:${b64_FOUND}>:b64::core> # this brought in by Pantheios
+			CLASP::core
+			libCLImate::core
 			Pantheios::Pantheios.core
 			Pantheios::Pantheios.fe.all
 			Pantheios::Pantheios.be.AnsiConsole
 			Pantheios::Pantheios.bec.AnsiConsole
 			Pantheios::Pantheios.util
+			recls::core
 			$<$<BOOL:${shwild_FOUND}>:shwild::core>
 			$<$<STREQUAL:${STLSOFT_INCLUDE_DIR},>:STLSoft::STLSoft>
 			xTests::core
@@ -69,11 +72,14 @@ function(define_example_program program_name entry_point_source_name)
 		PRIVATE
 			core
 			# $<$<BOOL:${b64_FOUND}>:b64::core> # this brought in by Pantheios
+			CLASP::core
+			libCLImate::core
 			Pantheios::Pantheios.core
 			Pantheios::Pantheios.fe.all
 			Pantheios::Pantheios.be.AnsiConsole
 			Pantheios::Pantheios.bec.AnsiConsole
 			Pantheios::Pantheios.util
+			recls::core
 			$<$<BOOL:${shwild_FOUND}>:shwild::core>
 			$<$<STREQUAL:${STLSOFT_INCLUDE_DIR},>:STLSoft::STLSoft>
 	)
